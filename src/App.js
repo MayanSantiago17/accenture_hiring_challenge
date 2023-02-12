b9b9a46838cf2320077b6d855fd51477fdb56d1d
MayanSantiago17 <mayansantiago1703@gmail.com>
2023-02-12 10:32:23 -0600

    init accenture hiring challenge

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Accenture Hiring Challenge
         </a>
       </header>
     </div>
